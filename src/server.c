e83bd3965176ad7a6c9884f55384833e7cf40462
Benjamin Chiang <chiangbenjamin991@gmail.com>
2025-03-28 22:22:47 -0700

    feat(args): add address and port options

--- src/server.c
+++ src/server.c
@@ -10,12 +10,14 @@
 
 typedef struct
 {
-    bool debug;
+    char     *address;
+    in_port_t port;
+    bool      debug;
 } arguments_t;
 
 static _Noreturn void usage(const char *binary_name, int exit_code, const char *message);
 static void           get_arguments(arguments_t *args, int argc, char *argv[]);
-static void           validate_arguments(const char *binary_name, arguments_t *args);
+static void           validate_arguments(const char *binary_name, const arguments_t *args);
 
 int main(int argc, char *argv[])
 {
@@ -44,34 +46,50 @@ static _Noreturn void usage(const char *binary_name, int exit_code, const char *
         fprintf(stderr, "%s\n\n", message);
     }
 
-    fprintf(stderr, "Usage: %s [-h] [-d]\n", binary_name);
+    fprintf(stderr, "Usage: %s [-h] [-d] -a <address> -p <port>\n", binary_name);
     fputs("Options:\n", stderr);
-    fputs("  -h, --help    Display this help message\n", stderr);
-    fputs("  -d, --debug   Enables the debug mode\n", stderr);
+    fputs("  -a, --address <address>   Address of the web server\n", stderr);
+    fputs("  -p, --port <port>         Port to bind to\n", stderr);
+    fputs("  -h, --help                Display this help message\n", stderr);
+    fputs("  -d, --debug               Enables the debug mode\n", stderr);
     exit(exit_code);
 }
 
 static void get_arguments(arguments_t *args, int argc, char *argv[])
 {
+    int err;
     int opt;
 
     static struct option long_options[] = {
-        {"debug", no_argument, NULL, 'd'},
-        {"help",  no_argument, NULL, 'h'},
-        {NULL,    0,           NULL, 0  }
+        {"address", required_argument, NULL, 'a'},
+        {"port",    required_argument, NULL, 'p'},
+        {"debug",   no_argument,       NULL, 'd'},
+        {"help",    no_argument,       NULL, 'h'},
+        {NULL,      0,                 NULL, 0  }
     };
 
-    while((opt = getopt_long(argc, argv, "hd", long_options, NULL)) != -1)
+    while((opt = getopt_long(argc, argv, "hda:p:", long_options, NULL)) != -1)
     {
         switch(opt)
         {
+            case 'a':
+                args->address = optarg;
+                break;
+            case 'p':
+                args->port = convert_port(optarg, &err);
+
+                if(err != 0)
+                {
+                    usage(argv[0], EXIT_FAILURE, "Port must be between 1 and 65535");
+                }
+                break;
             case 'd':
                 args->debug = (optarg == NULL);
                 break;
             case 'h':
                 usage(argv[0], EXIT_SUCCESS, NULL);
             case '?':
-                if(optopt != 'd')
+                if(optopt != 'a' && optopt != 'p' && optopt != 'd')
                 {
                     char message[UNKNOWN_OPTION_MESSAGE_LEN];
 
@@ -85,8 +103,15 @@ static void get_arguments(arguments_t *args, int argc, char *argv[])
     }
 }
 
-static void validate_arguments(const char *binary_name, arguments_t *args)
+static void validate_arguments(const char *binary_name, const arguments_t *args)
 {
-    UNUSED(binary_name);
-    UNUSED(args);
+    if(args->address == NULL)
+    {
+        usage(binary_name, EXIT_FAILURE, "You must provide an IPv4 or IPv6 address to connect to.");
+    }
+
+    if(args->port == 0)
+    {
+        usage(binary_name, EXIT_FAILURE, "You must provide an available port to connect to.");
+    }
 }
