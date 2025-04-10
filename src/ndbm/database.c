9ce2b0e525bbcef344290b9f50fa4c7842c393a9
Benjamin Chiang <chiangbenjamin991@gmail.com>
2025-04-10 09:42:25 -0700

    feat: remove unused db_fetch

--- src/ndbm/database.c
+++ src/ndbm/database.c
@@ -51,47 +51,6 @@ int db_insert(DBM *db, const char *key, const uint8_t *buf, size_t size, int *er
     return result;
 }
 
-uint8_t *db_fetch(DBM *db, const char *key, size_t *len, int *err)
-{
-    datum       fetched;
-    const_datum key_datum = MAKE_CONST_DATUM(key);
-
-    if(db == NULL || key == NULL)
-    {
-        seterr(EINVAL);
-        return NULL;
-    }
-
-    // Read data from db
-    fetched = dbm_fetch(db, *(datum *)&key_datum);
-    if(fetched.dptr == NULL)
-    {
-        seterr(EINVAL);
-        return NULL;
-    }
-
-    if(fetched.dsize < 0)
-    {
-        log_error("db_fetch_bytes::%s: Returned size is less than zero.\n", key);
-        seterr(ERANGE);
-        return NULL;
-    }
-
-    if((unsigned long)fetched.dsize > SIZE_MAX)
-    {
-        log_error("db_fetch_bytes::%s: Returned size is greater than int max.\n", key);
-        seterr(ERANGE);
-        return NULL;
-    }
-
-    if(len)
-    {
-        *len = (size_t)fetched.dsize;
-    }
-
-    return (uint8_t *)fetched.dptr;
-}
-
 int db_init(DBM **db, const char *filepath, int *err)
 {
     char *database_name = strdup(filepath);
