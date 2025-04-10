6f2d538d07afd3a610980b5749fd7361d46494d4
Benjamin Chiang <chiangbenjamin991@gmail.com>
2025-04-10 02:57:07 -0700

    refactor: remove unused function declaration

--- include/handlers.h
+++ include/handlers.h
@@ -8,7 +8,6 @@
 void    handle_client_connect(int sockfd, app_state_t *app, const char *libhttp_filepath);
 ssize_t handle_client_data(int connfd);
 
-ssize_t handle_workers(worker_t *workers, size_t nworkers, app_state_t *app);
 ssize_t handle_worker_connect(const worker_t *worker, int fd);
 ssize_t handle_worker_disconnect(worker_t *worker, app_state_t *app);
 
